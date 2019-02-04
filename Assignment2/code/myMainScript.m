42d34d3348abbd9c9ebfe4b887b78738ad65608f
AnshKhurana <anshkhurana1507@gmail.com>
2019-02-05 00:13:03 +0530

    Started with defining functions

---
+++ Assignment2/code/myMainScript.m
@@ -0,0 +1,11 @@
+%% Assignment 2, Image Denoising
+%  CS736: Medical Image Computing, IIT Bombay (Spring 2019)
+%  Dhruv Shah and Ansh Khurana
+%%
+% 
+% Info
+% 
+
+clc; clear all; close all;
+% warning('off', 'all');
+
