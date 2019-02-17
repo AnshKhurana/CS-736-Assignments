a1bbcf75dacee2d7cde19182f1d3845d4b8cae86
AnshKhurana <anshkhurana1507@gmail.com>
2019-02-17 21:30:25 +0530

    For pushing branch

---
+++ Assignment3/code/myIntegration.m
@@ -0,0 +1,8 @@
+function z = myIntegration(img, t, theta, step)
+% Returns the integral of interpolated values along the line L(t, theta)
+% Calculating the lower Darboux sum for convenience
+
+lower_limit = 
+upper_limit = 
+
+
---
+++ Assignment3/code/myMainScript.m
@@ -0,0 +1,9 @@
+%% Assignment 3, CT Imaging
+%  CS736: Medical Image Computing, IIT Bombay (Spring 2019)
+%  Dhruv Shah and Ansh Khurana
+%%
+% <Description>
+
+warning('off', 'all');
+clc; clear all; close all;
+%% Question 1 
