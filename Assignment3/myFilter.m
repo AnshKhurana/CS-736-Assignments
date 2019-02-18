938906a941465397c9ca5744dc9d42a34d1ce01a
Dhruv Ilesh Shah <dhruv.ilesh@gmail.com>
2019-02-19 00:59:27 +0530

    Magnitute of filter fixed

--- Assignment3/myFilter.m
+++ Assignment3/myFilter.m
@@ -5,31 +5,25 @@ function filt_rad = myFilter(imrad, type, window)
 
 if (nargin < 2)
     type = 'Ram-Lak';
-    window = 0.5;
+    window = 1;
 end
 
-w = floor(size(imrad, 1) / 2);
-wmax = w; % Observed; confirm
-L = floor(wmax * window);
-lin = [1:w]; filt = lin;
+fft_n = 1024;
+filt = 2 * (0:(fft_n/2)) / fft_n;
+w = 2 * pi * (0:size(filt, 2)-1) / fft_n;
+L = window;
+
 if strcmp(type, 'Ram-Lak')
-    filt(1:L) = 1;
-    filt(L+1:w) = 0;
+    % Do nothing
 elseif strcmp(type, 'Shepp-Logan')
-    filt(1:L) = sin(0.5 * pi * [1:L] / L) ./ (0.5 * pi * [1:L] / L);
-    filt(L+1:w) = 0;
+    filt(2:end) = filt(2:end) .* (sin(w(2:end)/(2*L))./(w(2:end)/(2*L)));
 elseif strcmp(type, 'Cosine')
-    filt(1:L) = cos(0.5 * pi * [1:L] / L);
-    filt(L+1:w) = 0;
+    filt(2:end) = filt(2:end) .* cos(w(2:end)/(2*L));
 end
 
-lin = lin.*filt;
-if numel(lin)*2 == size(imrad, 1)
-    disp('Error'); return;
-    % lin = [lin lin(end:-1:1)];
-elseif numel(lin)*2 == size(imrad, 1)-1
-    lin = [0 lin lin(end:-1:1)];
-end
+filt(w > pi*L) = 0; % Window
+filt = [filt'; filt(end-1:-1:2)'];
 
-filt_imrad = fft(imrad) .* repmat(lin', [1, size(imrad, 2)]);
+filt_imrad = fft(imrad, fft_n) .* repmat(filt, [1, size(imrad, 2)]);
 filt_rad = ifft(filt_imrad);
+filt_rad(size(imrad, 1)+1:end, :) = []; % Truncate projections
--- Assignment3/working.m
+++ Assignment3/working.m
@@ -11,13 +11,35 @@ imdec_BP = iradon(imrad, theta, 'linear', 'None', 1, imsize);
 
 modes = {'Ram-Lak', 'Cosine', 'Shepp-Logan'};
 
+%% Verifying against Matlab implementations
+
 for filt_type = modes
     imfilt = myFilter(imrad, char(filt_type), 1);
     imdec_ours = iradon(imfilt, theta, 'linear', 'None', 1, imsize);
     imdec_matlab = iradon(imrad, theta, 'linear', char(filt_type), 1, imsize);
     figure;
-    subplot(1, 2, 1); imagesc(imdec_ours);
-    title('Our Implementation');
-    subplot(1, 2, 2); imagesc(imdec_matlab);
-    title('Matlab iradon FBP')
+    subplot(1, 2, 1); imagesc(imdec_ours); colorbar;
+    title(strcat(char(filt_type), '(Ours)'));
+    subplot(1, 2, 2); imagesc(imdec_matlab); colorbar;
+    title(strcat(char(filt_type), '(Matlab)'));
+end
+
+%% Part (a)
+
+figure('Position', [100, 100, 1000, 400]);
+subplot(1, 2, 1); imagesc(img);
+title('Original');
+subplot(1, 2, 2); imagesc(iradon(imrad, theta, 'linear', 'None', 1, imsize));
+title('Unfiltered Back Projection')
+
+for filt_type = modes
+    imfilt1 = myFilter(imrad, char(filt_type), 1);
+    imdec_ours1 = iradon(imfilt1, theta, 'linear', 'None', 1, imsize);
+    imfilt2 = myFilter(imrad, char(filt_type), 0.5);
+    imdec_ours2 = iradon(imfilt2, theta, 'linear', 'None', 1, imsize);
+    figure('Position', [100, 100, 1000, 400]);
+    subplot(1, 2, 1); imagesc(imdec_ours1);
+    title(strcat(char(filt_type), '(L = w_{max})'));
+    subplot(1, 2, 2); imagesc(imdec_ours2);
+    title(strcat(char(filt_type), '(L = w_{max} / 2)'));
 end
