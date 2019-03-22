51b981e17f937e3e09ab1b224820e76b8889d36e
AnshKhurana <anshkhurana1507@gmail.com>
2019-03-23 04:30:24 +0530

    Working code, fix report titles

--- Assignment4/1/code/costfn.m
+++ Assignment4/1/code/costfn.m
@@ -1,10 +1,12 @@
-function J = costfn(w, q, y, memberships, means, bias, mask)
+function J = costfn(w, q, y, memberships, means, bias, mask, K)
 
 % J_present = costfn(w, q, y, memberships,  means, bias, mask);
 J = 0;
-[M, N] = size(y);
-for i = 1 : M*N
-    for k = 1:K
-        J = J + sum((memberships(k)().^q).*((y - memberships(k)*bias).^2));  
-    end
+% imshow(y);
+for k = 1:K
+    % Have to fix, add convolution operation
+    temp = y.*y - 2 .* y .* (conv2(means(k).*bias, w, 'same') .*mask) + (conv2((means(k) * means(k)).* (bias.^2), w, 'same') .* mask);
+    temp = temp .* mask;
+    c =  sum((memberships(:, :, k).^q).*temp, 'all');
+    J = J + c;  
 end
